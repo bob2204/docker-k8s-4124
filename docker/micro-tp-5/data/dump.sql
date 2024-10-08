ae153bdfff83142a93d5095d4feb0913c2a73c33
Bruno Guérin <bguerin@dawan.fr>
2024-10-08 16:22:40 +0200

    [NEW] Data SQL

---
+++ docker/micro-tp-5/data/dump.sql
@@ -0,0 +1,22 @@
+CREATE TABLE t1 (
+    id int primary key,
+    stagiaire varchar(50) not null
+);
+
+INSERT INTO t1 VALUES
+   (1,'mohamed'),
+   (2,'lucas'),
+   (3,'allan'),
+   (4,'audric'),
+   (5,'elliott'),
+   (6,'eddy'),
+   (7,'élodie'),
+   (8,'hicham'),
+   (9,'jocelyn'),
+   (10,'sandess'),
+   (11,'levy'),
+   (12,'ramdane'),
+   (13,'théo'),
+   (14,'timothé'),
+   (15,'valentin'),
+   (16,'yacine');
